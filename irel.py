4f629166f0d1b56443d181e36315d98323b3fcf3
Sean Takats <sean@takats.org>
2018-01-18 17:29:18 +0100

    ANOM now uses OpenSeadragon for a viewer (and JPEG instead of JP2000), breaking all old logic. This is a first rough pass but should work with the new images.

--- irel.py
+++ irel.py
@@ -1,11 +1,10 @@
 #!/usr/bin/env python3
 # -*- coding: UTF-8 -*-
 # Sean Takats
-# Note: requires ImageMagick with JP2 (jpeg 2000) support
-# e.g. with homebrew on the mac, 'brew install imagemagick --with-jp2'
-# this might be included by default now...
+# Note: no longer requires ImageMagick with JP2 (jpeg 2000) support
+# DZI / OpenSeadragon logic based on https://github.com/lovasoa/dezoomify
 
-import os, math, binascii, argparse, backoff, requests
+import os, math, binascii, argparse, backoff, requests, re, ast, json
 from urllib.parse import urlparse, quote
 from bs4 import BeautifulSoup
 
@@ -18,110 +17,109 @@ arkid = args.arkid
 directory = args.directory
 os.chdir(directory)
 
-# each ANOM dossier has a unique ID, e.g. ark:/61561/up424fz222yu
-
-# use backoff and requests to handle timeouts and retry gracefully
-
 @backoff.on_exception(backoff.expo,
                       requests.exceptions.RequestException,
                       max_tries=8)
 def get_url(url):
     return requests.get(url)
 
-baseurl = 'http://anom.archivesnationales.culture.gouv.fr/'
+baseurl = 'http://anom.archivesnationales.culture.gouv.fr'
 
-url = baseurl + arkid
+url = baseurl + '/' + arkid
 soup = BeautifulSoup(get_url(url).text, "lxml")
 
-contentlink = soup.find('a', attrs={'target':'pix2web'})
+contentlink = soup.find('a', href=re.compile('^/osd/\?dossier='))
 dossier = contentlink.get('href')
-dossier = (quote(dossier, '+?=&/'))
 
 print ('Loading ' + arkid)
-
 newurl = baseurl+dossier
-
 soup = BeautifulSoup(get_url(newurl).text, "lxml")
 
-docURL = soup.find('param', attrs={'name':'docUrl'}).get('value')
-dossierURL = soup.find('param', attrs={'name':'from'}).get('value')
-firstimage = int(soup.find('param', attrs={'name':'min'}).get('value'))
-lastimage = int(soup.find('param', attrs={'name':'max'}).get('value'))
-digits = soup.find('param', attrs={'name':'nbnum'}).get('value')
-padding = '%0'+digits+'d'
+pattern = re.compile(r'initViewer')
+script_tag = soup.find('script', text=pattern)
 
-totalimages = lastimage + 1 - firstimage
+dzilist = script_tag.contents[0]
+dzilist = dzilist.split('[', 1)[1].split(']')[0]
+dzilist = "["+dzilist+"]"
+dzilist = ast.literal_eval(dzilist)
 
+totalimages = len(dzilist)
 print ('Dossier contains ' + str(totalimages) + ' pages')
 
 # get all the image pages
-
 pdfcommand = 'convert \('
 pdfname = url.split('ark:/61561/')[1] + ".pdf"
-i = 0
 
-for image in range (firstimage, lastimage+1): # pull each image page
-    
-    xmlrequest = baseurl+docURL+dossierURL+padding % image+'_img.xml'
-    
+i = 0
+tempjpgs = ""
+
+for image in dzilist: # pull each image page
+
+    temptiles = ""
+
+    image = '"'+image+'"'
+    image = json.loads(image)
+    xmlrequest = baseurl + image
     path = urlparse(xmlrequest).path
     path = os.path.dirname(path)
-    
     soup = BeautifulSoup (get_url(xmlrequest).text, "lxml")
 
     i += 1
     print ('Grabbing image ' + str(i) + ' of ' + str(totalimages))
 
-    imgurltemplate = soup.atiledimage['tilestreamspectemplate']
-    
-    imgsize = soup.layers.apyramidlayer.size
-    tilesize = soup.layers.apyramidlayer.tilesize
-
+    tileimg = soup.image
+    imgsize = soup.image.size
+    imgformat = str(tileimg.get('format'))
     imgwidth = int(imgsize.get('width'))
     imgheight = int(imgsize.get('height'))
-    tilewidth = int(tilesize.get('width'))
-    tileheight = int(tilesize.get('height'))
+    tilesize = int(tileimg.get('tilesize'))
+    overlap = int(tileimg.get('overlap'))
+    tileswide = math.ceil(imgwidth/tilesize)
+    tileshigh = math.ceil(imgheight/tilesize)
+    zoom = math.ceil(math.log2(max(imgwidth, imgheight)))
 
-    tileswide = math.ceil(imgwidth/tilewidth)
-    tileshigh = math.ceil(imgheight/tileheight)
+    # replace .dzi extension with _files/
+    tilebase = xmlrequest.rsplit( '.', 1 )[ 0 ] + '_files/'
 
-    filename = dossierURL.split('/')[1] + str(image) + ".jpg"
+    filename = urlparse(xmlrequest).path
+    filename = filename.rsplit( '.', 1 )[ 0 ]+'.'+imgformat
+    filename = os.path.basename(filename)
+    filename = os.path.join(directory, filename)
 
-    jpgcommand = "convert -strip -interlace Plane -quality 85%"
+    jpgcommand = "convert -strip "
 
     for x in range (tileswide): # download each tile image and build imagemagick command
         jpgcommand += " \( "
         for y in range (tileshigh):
             
-            imgURL = baseurl + path + '/' + imgurltemplate % (y, x)
-            
+            imgURL = tilebase + str(zoom) + '/' + str(x) + '_' + str(y) + '.' + imgformat
             # file to be written to
             file = urlparse(imgURL).path
             file = os.path.basename(file)
             file = os.path.join(directory, file)
 
-            jpgcommand += " " + "'" + file + "'" 
- 
-            response = get_url(imgURL)
-            hexcontent = binascii.hexlify(response.content)
-            hexcontent = hexcontent[hexcontent.find(b'ff4fff51'):] # strip headers before jpeg2000 codestream
+            jpgcommand += " \( " + "'" + file + "'" + " -shave "+str(overlap)+"x"+str(overlap)+" \)"
 
-            content = binascii.unhexlify(hexcontent)
+            response = get_url(imgURL)
+
             f = open(file, 'wb')
-            f.write(content)
+            f.write(response.content)
             f.close()
+
+            temptiles = temptiles + file + " "
 
         jpgcommand += " -append \)"
 
+    print ("Combining tiles and saving as a temporary JPG")
     jpgcommand += " +append " + "'" + os.path.join(directory, filename) + "'"
     pdfcommand += " "  + "'" + os.path.join(directory, filename) + "'"
-    print ("Combining tiles and saving as a temporary JPEG")
     os.system(jpgcommand) # pasting the tiles together and saving as a jpg
-    os.system('rm *TILE*') # removing temporary tile images
+    print ('Cleaning up tiles')
+    os.system('rm ' + temptiles) # removing temporary tile images
+    tempjpgs = tempjpgs + filename + " "
 
 pdfcommand += " \) " + "'" + pdfname + "'"
 print ('Saving images in a single PDF')
 os.system(pdfcommand) # embedding all jpgs into a single pdf
-print ('Cleaning up')
-os.system('rm *.jpg') # deleting the temporary jpgs
-                        
+print ('Cleaning up JPGs')
+os.system('rm ' + tempjpgs) # deleting the temporary jpgs
